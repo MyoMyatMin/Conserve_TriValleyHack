c66b420b5f1591c588f465537c92fa5be8828921
Noel Paing Oak Soe <u6530183@au.edu>
2024-05-14 15:08:44 +0700

    Implemented Q & A Session
    Modified GlobalProvider.js
    Modified survey.js
    Added images and icons to assets
    Added MorningSurvey.jsx, AfternoonSurvey.jsx, NightSurvey.jsx to components
    Installed moment library

--- reactnative/app/(tabs)/survey.jsx
+++ reactnative/app/(tabs)/survey.jsx
@@ -1,13 +1,219 @@
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  Alert,
+  Image,
+  Modal,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native'
 import React from 'react'
+import { useState } from 'react'
+import { icons } from '../../constants'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useGlobalContext } from '../../context/GlobalProvider'
+import MorningSurvey from '../components/MorningSurvey'
+import AfternoonSurvey from '../components/AfternoonSurvey'
+import NightSurvey from '../components/NightSurvey'
 
+const getButtonColor = (surveyState) => {
+  switch (surveyState) {
+    case 'completed':
+      return '#008000' // green
+    case 'pending':
+      return '#ffff00' // yellow
+    default:
+      return '#007AFF' // blue
+  }
+}
 const Survey = () => {
+  const [showSurvey, setShowSurvey] = useState(false)
+  const [showAfternoon, setShowAfternoon] = useState(false)
+  const [showNight, setShowNight] = useState(false)
+  const { morning, afternoon, night, setMorning, setAfternoon, setNight } =
+    useGlobalContext()
+
+  const handleSurveyPress = (survey) => {
+    // console.log(morning)
+    if (survey === 'morning' && morning === 'ready') {
+      setShowSurvey(true)
+    } else if (
+      survey === 'afternoon' &&
+      morning === 'completed' &&
+      afternoon === 'ready'
+    ) {
+      setShowAfternoon(true)
+    } else if (
+      survey === 'afternoon' &&
+      morning !== 'completed' &&
+      afternoon === 'ready'
+    ) {
+      Alert.alert('Need to finish Morning First!')
+    } else if (
+      survey === 'night' &&
+      morning === 'completed' &&
+      afternoon === 'completed' &&
+      night === 'ready'
+    ) {
+      setShowNight(true)
+    } else if (
+      survey === 'night' &&
+      (morning !== 'completed' || afternoon !== 'completed') &&
+      night === 'ready'
+    ) {
+      Alert.alert('Need to finish previous survey fist!')
+    } else if (
+      morning === 'completed' ||
+      afternoon === 'completed' ||
+      night === 'completed'
+    ) {
+      Alert.alert('Already Taken!')
+    } else if (
+      morning === 'pending' ||
+      afternoon === 'pending' ||
+      night === 'pending'
+    ) {
+      Alert.alert('Can not take yet')
+    }
+  }
   return (
-    <SafeAreaView className="bg-primary h-full">
-      <Text className="text-2xl text-secondary">Survey</Text>
+    <SafeAreaView className='bg-primary h-full'>
+      <View style={styles.container}>
+        <Text style={[styles.text, { marginBottom: 50 }]}>
+          How is your Day?
+        </Text>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: '#26D6AF' }]}
+          onPress={() => handleSurveyPress('morning')}
+        >
+          <View style={styles.buttonView}>
+            <Image source={icons.morningIcon} />
+            <Text style={styles.buttonText}>Morning</Text>
+            {morning === 'completed' && <Image source={icons.completedIcon} />}
+            {morning === 'pending' && <Image source={icons.pendingIcon} />}
+            {morning === 'ready' && <Image source={icons.readyIcon} />}
+          </View>
+        </TouchableOpacity>
+        <Modal visible={showSurvey} animationType='slide'>
+          <View style={styles.modalContainer}>
+            <MorningSurvey />
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setShowSurvey(false)}
+            >
+              <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: '#615B7F' }]}
+          onPress={() => handleSurveyPress('afternoon')}
+        >
+          <View style={styles.buttonView}>
+            <Image source={icons.afternoonIcon} />
+            <Text style={styles.buttonText}>Afternoon</Text>
+            {afternoon === 'completed' && (
+              <Image source={icons.completedIcon} />
+            )}
+            {afternoon === 'pending' && <Image source={icons.pendingIcon} />}
+            {afternoon === 'ready' && <Image source={icons.readyIcon} />}
+          </View>
+        </TouchableOpacity>
+        <Modal visible={showAfternoon} animationType='slide'>
+          <View style={styles.modalContainer}>
+            <AfternoonSurvey />
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setShowAfternoon(false)}
+            >
+              <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: '#271D3B' }]}
+          onPress={() => handleSurveyPress('night')}
+        >
+          <View style={styles.buttonView}>
+            <Image source={icons.nightIcon} />
+            <Text style={styles.buttonText}>Night</Text>
+            {night === 'completed' && <Image source={icons.completedIcon} />}
+            {night === 'pending' && <Image source={icons.pendingIcon} />}
+            {night === 'ready' && <Image source={icons.readyIcon} />}
+          </View>
+        </TouchableOpacity>
+        <Modal visible={showNight} animationType='slide'>
+          <View style={styles.modalContainer}>
+            <NightSurvey />
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setShowNight(false)}
+            >
+              <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 50,
+    marginTop: 50,
+  },
+  buttonView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    padding: 10,
+    borderRadius: 20,
+    width: 350,
+    height: 60,
+    marginBottom: 25,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    position: 'relative',
+  },
+  closeButton: {
+    width: 35,
+    height: 35,
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    borderColor: 'white',
+    padding: 5,
+    borderRadius: 50,
+    position: 'absolute',
+    bottom: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  closeButtonText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+})
 
 export default Survey
--- reactnative/context/GlobalProvider.js
+++ reactnative/context/GlobalProvider.js
@@ -1,19 +1,170 @@
-import { useContext, useState, createContext } from "react";
+import { useContext, useState, createContext, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import moment from 'moment'
 
-const GlobalContext = createContext();
+const saveStates = async (states) => {
+  try {
+    await AsyncStorage.setItem('states', JSON.stringify(states))
+    console.log('updated the states')
+  } catch (e) {
+    console.log('Failed to save states:', e)
+  }
+}
+const loadStates = async () => {
+  try {
+    const statesJson = await AsyncStorage.getItem('states')
+    if (statesJson) {
+      return JSON.parse(statesJson)
+    } else {
+      return {
+        objmorning: 'pending',
+        objafternoon: 'pending',
+        objnight: 'pending',
+      }
+    }
+  } catch (e) {
+    console.log('Failed to load states:', e)
+  }
+}
 
-export const useGlobalContext = () => useContext(GlobalContext);
+const loadDay = async () => {
+  try {
+    const storedData = await AsyncStorage.getItem('previousDate')
+    // console.log(storedData)
+    return storedData
+  } catch (e) {
+    console.log('Failed to load day')
+  }
+}
+
+const GlobalContext = createContext()
+export const useGlobalContext = () => useContext(GlobalContext)
 const GlobalProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  // const [states, setStates] = useState()
+  const [morning, setMorning] = useState('pending')
+  const [afternoon, setAfternoon] = useState('pending')
+  const [night, setNight] = useState('pending')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [previousDate, setPreviousDate] = useState(null)
+
+  const loadInitialStates = async () => {
+    const initialStates = await loadStates()
+    setMorning(initialStates.objmorning)
+    setAfternoon(initialStates.objafternoon)
+    setNight(initialStates.objnight)
+    return initialStates
+  }
+
+  const loadInitialDays = async () => {
+    const initialDate = moment()
+    // console.log('initial')
+    // console.log(initialDate)
+    const storedDate = await loadDay()
+    setPreviousDate(storedDate ? moment(storedDate) : initialDate)
+    // console.log(previousDate)
+    return storedDate
+  }
+
+  // useEffect(() => {
+  //   loadInitialStates()
+  // }, [])
+  useEffect(() => {
+    loadInitialDays()
+  }, [])
+  useEffect(() => {
+    const updateStates = async () => {
+      const currentHour = new Date().getHours()
+      let prevDate = await loadInitialDays()
+      const currentDate = moment()
+      // console.log(currentDate.date())
+      const isNewDay = currentDate.date() != moment(prevDate).date()
+
+      console.log(isNewDay)
+
+      if (isNewDay) {
+        let myobj = await loadInitialStates()
+        myobj.objmorning = 'pending'
+        myobj.objafternoon = 'pending'
+        myobj.objnight = 'pending'
+        setMorning(myobj.objmorning)
+        setAfternoon(myobj.objafternoon)
+        setNight(myobj.objnight)
+        setPreviousDate(currentDate)
+        await AsyncStorage.setItem('previousDate', currentDate.toISOString())
+        await saveStates(myobj)
+      } else {
+        let myobj = await loadInitialStates()
+        let newMorning = 'pending'
+        let newAfternoon = 'pending'
+        let newNight = 'pending'
+
+        if (
+          currentHour > 0 &&
+          currentHour <= 23 &&
+          myobj.objmorning != 'completed'
+        ) {
+          newMorning = 'ready'
+        } else if (myobj.objmorning == 'completed') {
+          newMorning = 'completed'
+        } else {
+          newMorning = 'pending'
+        }
+
+        if (currentHour >= 12 && myobj.objafternoon != 'completed') {
+          newAfternoon = 'ready'
+        } else if (myobj.objafternoon === 'completed') {
+          newAfternoon = 'completed'
+        } else {
+          newAfternoon = 'pending'
+        }
+
+        if (currentHour >= 18 && myobj.objnight != 'completed') {
+          newNight = 'ready'
+        } else if (myobj.objnight == 'pending') {
+          newNight = 'pending'
+        } else if (myobj.objnight === 'completed') {
+          newNight = 'completed'
+        }
+
+        myobj.objmorning = newMorning
+        myobj.objafternoon = newAfternoon
+        myobj.objnight = newNight
+        setMorning(newMorning)
+        setAfternoon(newAfternoon)
+        setNight(newNight)
+
+        await saveStates(myobj)
+      }
+    }
+
+    updateStates()
+    const interval = setInterval(updateStates, 60 * 60 * 1000) // update every minute
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <GlobalContext.Provider
-      value={{ isLoggedIn, setIsLoggedIn, user, setUser, isLoading }}
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        user,
+        setUser,
+        isLoading,
+        morning,
+        setMorning,
+        afternoon,
+        setAfternoon,
+        night,
+        setNight,
+        saveStates,
+        loadStates,
+      }}
     >
       {children}
     </GlobalContext.Provider>
-  );
-};
+  )
+}
 
-export default GlobalProvider;
+export default GlobalProvider
