f787b85ce2349a03acde6bcb70f73ed87eacee8b
Noel Paing Oak Soe <u6530183@au.edu>
2024-05-23 23:54:26 +0700

    Feat: Implemented Notification with rooms to improve. Wip: Fixed UI errors in Survey page.

--- reactnative/app/(auth)/sign-in.jsx
+++ reactnative/app/(auth)/sign-in.jsx
@@ -1,96 +1,111 @@
-import { useState } from "react";
-import { Link, router } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import CustomButton from "../components/CustomButton";
-import FormField from "../components/FormField";
-import { useGlobalContext } from "../../context/GlobalProvider";
+import { useState } from 'react'
+import { Link, router } from 'expo-router'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+  Alert,
+  Image,
+  StatusBar,
+} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import CustomButton from '../components/CustomButton'
+import FormField from '../components/FormField'
+import { useGlobalContext } from '../../context/GlobalProvider'
+import { images } from '../../constants'
 
 const SignIn = () => {
-  const [isSubmitting, setSubmitting] = useState(false);
-  const { setUser, setIsLoggedIn } = useGlobalContext();
+  const [isSubmitting, setSubmitting] = useState(false)
+  const { setUser, setIsLoggedIn } = useGlobalContext()
   const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
+    email: '',
+    password: '',
+  })
 
   const submit = async () => {
     try {
-      const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+      const apiUrl = process.env.EXPO_PUBLIC_API_URL
 
       const res = await fetch(`${apiUrl}/api/users/signin`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(form),
-      });
+      })
 
-      const data = await res.json();
+      const data = await res.json()
       if (data.error) {
-        Alert.alert("Error", data.error);
-        return;
+        Alert.alert('Error', data.error)
+        return
       }
-      await AsyncStorage.setItem("userData", JSON.stringify(data.token));
-      setUser(JSON.stringify(data));
-      setIsLoggedIn(true);
-      router.replace("/home");
+      await AsyncStorage.setItem('userData', JSON.stringify(data.token))
+      setUser(JSON.stringify(data))
+      setIsLoggedIn(true)
+      router.replace('/home')
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
-  };
+  }
 
   return (
-    <SafeAreaView className="bg-gray-500 h-full">
+    <SafeAreaView className='bg-primary h-full'>
       <ScrollView>
         <View
-          className="w-full flex justify-center h-full px-4 my-6"
+          className='w-full flex justify-center items-center h-full px-4 my-6'
           style={{
-            minHeight: Dimensions.get("window").height - 100,
+            minHeight: Dimensions.get('window').height - 100,
           }}
         >
-          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[150px] h-[100px]'
+          />
+          <Text className='text-2xl font-semibold text-white mt-10 font-psemibold'>
             Sign In to Converse
           </Text>
 
           <FormField
-            title="Email"
+            title='Email'
             value={form.email}
             handleChangeText={(e) => setForm({ ...form, email: e })}
-            otherStyles="mt-7"
-            keyboardType="email-address"
+            otherStyles='mt-7'
+            keyboardType='email-address'
           />
 
           <FormField
-            title="Password"
+            title='Password'
             value={form.password}
             handleChangeText={(e) => setForm({ ...form, password: e })}
-            otherStyles="mt-7"
+            otherStyles='mt-7 '
           />
 
           <CustomButton
-            title="Sign In"
+            title='Sign In'
             handlePress={submit}
-            containerStyles="mt-7"
+            containerStyles='mt-7 w-full'
             isLoading={isSubmitting}
           />
 
-          <View className="flex justify-center pt-5 flex-row gap-2">
-            <Text className="text-lg text-gray-100 font-pregular">
+          <View className='flex justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>
               Don't have an account?
             </Text>
             <Link
-              href="/sign-up"
-              className="text-lg font-psemibold text-secondary"
+              href='/sign-up'
+              className='text-lg font-psemibold text-secondary'
             >
               SignUp
             </Link>
           </View>
         </View>
       </ScrollView>
+      <StatusBar backgroundColor={'#0B1A22'} style='light' />
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
--- reactnative/app/components/AfternoonSurvey.jsx
+++ reactnative/app/components/AfternoonSurvey.jsx
@@ -113,15 +113,16 @@ export default function AfternoonSurvey() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 20,
+    paddingHorizontal: 50,
     flex: 1,
-    width: '100%',
+    width: '450',
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   question: {
     color: 'white',
+    width: 350,
     padding: 40,
     paddingVertical: 50,
     textAlign: 'center',
